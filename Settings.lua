637aba7c449b2f37052b27ddba158f2cd280fa6c
YFNatey <yfnatey@gmail.com>
2025-07-16 12:10:59 -0700

    remove UI panels from NPC dialog when cinematic mode is enabled

--- Settings.lua
+++ Settings.lua
@@ -29,22 +29,12 @@ function CinematicCam:CreateSettingsMenu()
             name = "Black Bars Settings",
         },
         {
-            type = "checkbox",
-            name = "Enable Black Bars with Cinematic Mode",
-            tooltip = "When enabled, black bars will appear when toggling cinematic mode",
-            getFunc = function() return self.savedVars.letterboxEnabled end,
-            setFunc = function(value)
-                self.savedVars.letterboxEnabled = value
-                -- If cinematic mode is active, update letterbox visibility
-                if next(hiddenElements) then
-                    if value then
-                        self:ShowLetterbox()
-                    else
-                        self:HideLetterbox()
-                    end
-                end
+            type = "button",
+            name = "Toggle Black Bars",
+            func = function()
+                self:ToggleLetterbox()
             end,
-            width = "full",
+            width = "half",
         },
         {
             type = "checkbox",
@@ -118,89 +108,92 @@ function CinematicCam:CreateSettingsMenu()
         {
             type = "button",
             name = "Auto-calculate Size",
-            tooltip = "Automatically calculate letterbox size for your screen",
+            tooltip = "Automatically calculate black bar size for your screen",
             func = function()
                 self:CalculateLetterboxSize()
                 if not CinematicCam_LetterboxTop:IsHidden() then
                     CinematicCam_LetterboxTop:SetHeight(self.savedVars.letterboxSize)
                     CinematicCam_LetterboxBottom:SetHeight(self.savedVars.letterboxSize)
                 end
-                d("Letterbox size auto-calculated: " .. self.savedVars.letterboxSize)
             end,
             width = "half",
         },
         {
             type = "header",
-            name = "UI Controls",
+            name = "3rd Person Dialogue Settings",
         },
         {
-            type = "description",
-            text =
-            "Cinematic Camera hides standard UI elements like health bars, action bars, and compass when in cinematic mode. Use the commands below to toggle cinematic mode.",
-            width = "full",
-        },
-        {
-            type = "description",
-            text =
-            "Commands:\n/hideui - Toggle cinematic mode (hide UI elements)\n/letterbox - Toggle letterbox bars only\n/cinematic - Same as /hideui",
-            width = "full",
-        },
-        {
-            type = "button",
-            name = "Toggle Cinematic Mode",
-            tooltip = "Toggle UI hiding and letterbox bars",
-            func = function()
-                self:ToggleUI()
-            end,
-            width = "half",
-        },
-        {
-            type = "button",
-            name = "Toggle Letterbox Only",
-            tooltip = "Toggle just the letterbox bars",
-            func = function()
-                self:ToggleLetterbox()
-            end,
-            width = "half",
-        },
-
-        {
-            type = "header",
-            name = "Custom UI Elements",
-        },
-        {
-            type = "description",
-            text =
-            "Add custom UI elements to hide in cinematic mode. Enter the name of a UI element to add it to the list.",
-            width = "full",
-        },
-        {
-            type = "editbox",
-            name = "Add UI Element",
-            tooltip = "Enter the name of a UI element to hide in cinematic mode",
-            getFunc = function() return "" end,
-            setFunc = function(text)
-                if text and text ~= "" then
-                    -- Check if element exists
-                    local element = _G[text]
-                    if element then
-                        -- Add to saved variables
-                        self.savedVars.hideUiElements[text] = true
-                        d("Added UI element to hide: " .. text)
-                    else
-                        d("UI element not found: " .. text)
-                    end
-                end
+            type = "checkbox",
+            name = "3rd Person - NPC Dialogue",
+            tooltip = "Keep camera in 3rd person when talking to NPCs",
+            getFunc = function() return self.savedVars.forceThirdPersonDialogue end,
+            setFunc = function(value)
+                self.savedVars.forceThirdPersonDialogue = value
+                self:InitializeInteractionSettings()
             end,
             width = "full",
         },
         {
-            type = "button",
-            name = "Clear Custom UI Elements",
-            tooltip = "Clear all custom UI elements",
-            func = function()
-                self.savedVars.hideUiElements = {}
-                d("Cleared custom UI elements")
+            type = "checkbox",
+            name = "3rd Person - Vendors/Stores",
+            tooltip = "Keep camera in 3rd person when interacting with vendors and stores",
+            getFunc = function() return self.savedVars.forceThirdPersonVendor end,
+            setFunc = function(value)
+                self.savedVars.forceThirdPersonVendor = value
+                self:InitializeInteractionSettings()
+            end,
+            width = "full",
+        },
+        {
+            type = "checkbox",
+            name = "3rd Person - Banks",
+            tooltip = "Keep camera in 3rd person when using banks",
+            getFunc = function() return self.savedVars.forceThirdPersonBank end,
+            setFunc = function(value)
+                self.savedVars.forceThirdPersonBank = value
+                self:InitializeInteractionSettings()
+            end,
+            width = "full",
+        },
+        {
+            type = "checkbox",
+            name = "3rd Person - Quest Interactions",
+            tooltip = "Keep camera in 3rd person during quest interactions",
+            getFunc = function() return self.savedVars.forceThirdPersonQuest end,
+            setFunc = function(value)
+                self.savedVars.forceThirdPersonQuest = value
+                self:InitializeInteractionSettings()
+            end,
+            width = "full",
+        },
+        {
+            type = "checkbox",
+            name = "3rd Person - Crafting Stations",
+            tooltip = "Keep camera in 3rd person when using crafting stations",
+            getFunc = function() return self.savedVars.forceThirdPersonCrafting end,
+            setFunc = function(value)
+                self.savedVars.forceThirdPersonCrafting = value
+                self:InitializeInteractionSettings()
+            end,
+            width = "full",
+        },
+        {
+            type = "checkbox",
+            name = "Auto black bars During Dialogue",
+            tooltip = "Automatically show black bars during 3rd person dialogue",
+            getFunc = function() return self.savedVars.autoLetterboxDialogue end,
+            setFunc = function(value)
+                self.savedVars.autoLetterboxDialogue = value
+            end,
+            width = "full",
+        },
+        {
+            type = "checkbox",
+            name = "Auto Hide UI During Dialogue",
+            tooltip = "Automatically hide UI elements during 3rd person dialogue",
+            getFunc = function() return self.savedVars.autoHideUIDialogue end,
+            setFunc = function(value)
+                self.savedVars.autoHideUIDialogue = value
             end,
             width = "full",
         },
